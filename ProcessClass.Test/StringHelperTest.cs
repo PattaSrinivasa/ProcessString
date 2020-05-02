9ab50cada46a8e4f5066498ec91ca37fb6326895
PattaSrinivasa <64655754+PattaSrinivasa@users.noreply.github.com>
2020-05-02 16:18:15 +0100

    Added the missing files during initial commit

---
+++ ProcessClass.Test/StringHelperTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Processor;
+
+namespace Processor.Test
+{
+    [TestClass]
+    public class StringHelperTest
+    {
+        StringHelper _helper;
+
+        public StringHelperTest()
+        {
+            _helper = new StringHelper();
+        }
+
+        #region Test cases for GetMaxStringLength
+
+        [TestMethod]
+        public void GetMaxStringLength_ReturnsNullWhenNullStringPassed()
+        {
+            var result = _helper.GetMaxStringLength(null);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void GetMaxStringLength_Returns15CharactersForInputMoreThan15Chars()
+        {
+
+            var result = _helper.GetMaxStringLength("1234567890abcdefgh");
+
+            Assert.AreEqual(15, result.Length);
+        }
+
+        [TestMethod]
+        public void GetMaxStringLength_ReturnsLessCharactersFoInputBelow15Chars()
+        {
+
+            var result = _helper.GetMaxStringLength("1234567890");
+
+            Assert.IsTrue(result.Length <= 15);
+        }
+
+        #endregion
+
+        #region Test cases for ReplaceCharacters
+
+        [TestMethod]
+        public void ReplaceCharacters_ReturnsNullWhenNullStringPassed()
+        {
+
+            var result = _helper.ReplaceCharacter(null, "$", "£");
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [DataRow("ABcdDD$56f", null, "£", "ABcdDD$56f")] // oldChar null test 
+        [DataRow("ABcdDD$56f", "$", null, "ABcdDD$56f")] // newChar null test
+        public void ReplaceCharacters_ReturnsInputStringWhenNullPassedForOldOrNewChar(string input, string oldString, string newString, string expectedResult)
+        {
+
+           var result = _helper.ReplaceCharacter(input, oldString, newString);
+
+           Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void ReplaceCharacters_ReplaceDollarWithPound()
+        {
+
+            var result = _helper.ReplaceCharacter("ABcdDD$56f", "$", "£");
+
+            Assert.IsTrue(!result.Contains("$"));
+            Assert.AreEqual('£', result[6]);
+        }
+
+        #endregion
+
+        #region Test cases for ReplaceConsecutiveMultipleChars
+
+        [TestMethod]
+        public void ReplaceConsecutiveMultipleChars_ReturnsNullWhenNullStringPassed()
+        {
+
+            var result = _helper.ReplaceConsecutiveMultipleChars(null);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [DataRow("AAABBBbbbDDeee££456$", "ABbDe££456$")] // Replaces consecutive Alphabets
+        [DataRow("AAABBB111bbbDDeee££4456$", "AB111bDe££4456$")] // Do not replace consecutive numbers
+        [DataRow("AAABB***111bbbDDeee££4456$///!!", "AB***111bDe££4456$///!!")] // Do not replace consecutive special characters
+        public void ReplaceConsecutiveMultipleChars_ReplacesOnlyConsecutiveAlphabets(string input,string expectedOutput)
+        {
+            var result = _helper.ReplaceConsecutiveMultipleChars(input);
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+        #endregion
+    }
+}
---
+++ ProcessClass.Test/StringProcessorTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Processor;
+
+namespace Processor.Test
+{
+
+    [TestClass]
+    public class StringProcessorTest
+    {
+        IStringProcessor _processor;
+
+        public StringProcessorTest()
+        {
+            _processor = new StringProcessor();
+        }
+
+        #region Test cases for ProcessStringCollection
+
+        [TestMethod]
+        public void ProcessStringCollection_ReturnsEmptyListWhenNullIsPassed()
+        {
+
+            var result = _processor.ProcessStringCollection(null);
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        [DataRow("AAABB***111bbbDDeee££2256$///!!", "AB***111bDe££22")] // Removes consecutive duplicate alphabets from the string
+        [DataRow("AAABB_***111bbbDDeee££2256$///!!", "AB***111bDe££22")] // Removes underscores from the string
+        [DataRow("AAABB4***111bbbDDeee££2256$///!!", "AB***111bDe££22")] // Removes number 4 from the string
+        [DataRow("AAABB$***111bbbDDeee££2256$///!!", "AB£***111bDe££2")] // Replaces dollar with pound in the string
+        public void ProcessStringCollection_RemovesAndReplacesCharsAsExpected(string inputString, string expectedOutput)
+        {
+            List<string> input = new List<string>();
+
+            input.Add(inputString);
+
+            var result = _processor.ProcessStringCollection(input);
+
+            Assert.AreEqual(expectedOutput, result[0]);
+        }
+
+        [TestMethod]
+        public void ProcessStringCollection_ReturnsMax15Characters()
+        {
+
+            List<string> input = new List<string>();
+
+            input.Add("AAABB$***111bbbDDeee££2256$///!!");
+
+            var result = _processor.ProcessStringCollection(input);
+
+            Assert.AreEqual(15, result[0].Length);
+        }
+
+        [TestMethod]
+        public void ProcessStringCollection_ReturnsEmptyListForInputWithAllNumeral4()
+        {
+
+            List<string> input = new List<string>();
+
+            input.Add("44444444");
+
+            var result = _processor.ProcessStringCollection(input);
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        #endregion
+    }
+}
